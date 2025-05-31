5cfca1f02fbdc6a836417d1d72ed8bb44e29c2ca
nonesense-code <kaushalkadariya2062@gmail.com>
2025-05-31 16:48:56 +0545

    updated

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,3 @@ app.use("/users", userRouter);
 app.use("/products", productRouter);
 app.use("/api/cart", cartRouter);
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
